ac02f618df46315b8b794752cf68cfaeeb958664
Dion <ksundong@gmail.com>
2020-11-11 15:15:17 +0900

    feat: 저장된 이름 리스트화면 구현

--- lib/main.dart
+++ lib/main.dart
@@ -77,5 +77,26 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map(
+        (WordPair pair) {
+          return ListTile(
+            title: Text(pair.asPascalCase, style: _biggerFont),
+          );
+        },
+      );
+      final divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 }
